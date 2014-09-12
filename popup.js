6b6df7688ead303613a529a50962f848a97f4720
Matt <Matt@MBA.local>
2014-09-11 19:59:52 -0500

    Work

--- popup.js
+++ popup.js
@@ -1,39 +1,19 @@
-//Pomodoro Timer Script
+//Pomodoro Timer
+//Popup Script
 
 
 $(document).ready(function() {
 
-
-	var d = new Date()
-	var endworktime = d.getTime() + 3000000 // Add 50 minutes
-
-	var hours, minutes, seconds
-
-
-	function runclock() {
-		setInterval(function() {
-			var current_time = new Date().getTime();
-			var seconds_left = (endworktime - current_time) / 1000;
-
-			hours = parseInt(seconds_left / 3600);
-			seconds_left = seconds_left % 3600;
-
-			console.log("debugseconds: " + seconds_left);
-
-			minutes = parseInt(seconds_left / 60);
-			seconds = parseInt(seconds_left % 60);
-
-			var newtime = " " + hours + "h, " + minutes + "m, " + seconds + "s";
-
-			document.getElementbyId("clock").innerHTML = newtime;
-			// Is it more efficent to make this a var?
-
-		}, 1000)
-	}
+	// Set up the Message passing 
+	var port = chrome.runtime.connect({name: "timer"});
+	// Set up a listener that will update the clock
+	port.onMessage.addListener(function(msg) {
+			document.getElementById("clock").innerHTML = msg;
+	 	});
 
 
     $('button').click(function() {
-    	runclock();
+    	turnOnClock();
     });
 });
 
